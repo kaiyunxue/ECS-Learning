4dc216794f47c2c2c51c62b8402a267c94241628
KevinXue <kevinxue0239@gmail.com>
2019-01-31 17:34:48 -0500

    Initial

---
+++ Assets/Scripts/InputComponent.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputComponent : MonoBehaviour
+{
+    public float horizontal;
+    public float vertical;
+}
