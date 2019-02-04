ce21b2173cb0338f5998ba72805dffe71b553428
KevinXue <kevinxue0239@gmail.com>
2019-02-04 15:32:43 -0500

    ecs learning

--- Assets/Advanced/Boids/Scripts/SpawnRandomInSphereSystem.cs
+++ Assets/Advanced/Boids/Scripts/SpawnRandomInSphereSystem.cs
@@ -3,6 +3,7 @@ using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
+using UnityEngine;
 
 namespace Samples.Common
 {
@@ -21,7 +22,6 @@ namespace Samples.Common
         {
             m_MainGroup = GetComponentGroup(typeof(SpawnRandomInSphere), typeof(Position));
         }
-
         protected override void OnUpdate()
         {
             var uniqueTypes = new List<SpawnRandomInSphere>(10);
@@ -36,10 +36,12 @@ namespace Samples.Common
                 var entityCount = m_MainGroup.CalculateLength();
                 spawnInstanceCount += entityCount;
             }
+            Debug.Log(spawnInstanceCount);
 
             if (spawnInstanceCount == 0)
                 return;
 
+            //这一段不知道在干嘛，回来再看 貌似是在导数据
             var spawnInstances = new NativeArray<SpawnRandomInSphereInstance>(spawnInstanceCount, Allocator.Temp);
             {
                 int spawnIndex = 0;
@@ -50,7 +52,7 @@ namespace Samples.Common
 
                     if (m_MainGroup.CalculateLength() == 0)
                         continue;
- 
+
                     var entities = m_MainGroup.ToEntityArray(Allocator.TempJob);
                     var positions = m_MainGroup.ToComponentDataArray<Position>(Allocator.TempJob);
 
@@ -76,14 +78,14 @@ namespace Samples.Common
                 int spawnerIndex = spawnInstances[spawnIndex].spawnerIndex;
                 var spawner = uniqueTypes[spawnerIndex];
                 int count = spawner.count;
-                var entities = new NativeArray<Entity>(count,Allocator.Temp);
+                var entities = new NativeArray<Entity>(count, Allocator.Temp);
                 var prefab = spawner.prefab;
                 float radius = spawner.radius;
                 var spawnPositions = new NativeArray<float3>(count, Allocator.Temp);
                 float3 center = spawnInstances[spawnIndex].position;
                 var sourceEntity = spawnInstances[spawnIndex].sourceEntity;
 
-                GeneratePoints.RandomPointsInSphere(center,radius,ref spawnPositions);
+                GeneratePoints.RandomPointsInSphere(center, radius, ref spawnPositions);
 
                 EntityManager.Instantiate(prefab, entities);
 
@@ -93,7 +95,7 @@ namespace Samples.Common
                     {
                         Value = spawnPositions[i]
                     };
-                    EntityManager.SetComponentData(entities[i],position);
+                    EntityManager.SetComponentData(entities[i], position);
                 }
 
                 EntityManager.RemoveComponent<SpawnRandomInSphere>(sourceEntity);
